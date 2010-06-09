df319cce3358a22c8640421dd5bb7bce49253762
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2010-06-09 19:56:57 +0000

    validation fixes
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@212 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/metadata_table.py
+++ python_code/metadata_table.py
@@ -227,6 +227,7 @@ class MetadataTable(object):
     """
 
     _metadataFile = None
+    _log = []
 
     def __init__(self, metadataFile):
         global _metadataFile
@@ -278,6 +279,9 @@ class MetadataTable(object):
             
 
     def readMetadataFile(self):
+        # Log the current function
+        self._log.append('Entering readMetadataFile()...')
+        
         global _metadataFile
         column_factory = ColumnFactory(self._is_invalid)
         data_file = open(self._metadataFile, "rU")
@@ -294,8 +298,10 @@ class MetadataTable(object):
         # Create the header columns
         headers = reader.next()
         for column in headers:
+            self._log.append('Checking if column "%s" exists in column dictionary...' % column)
             try:    
                 if column in column_details:
+                    self._log.append('Column exists in dictionary.')
                     result = column_factory.createColumn(column, column_details[column][0], column_details[column][1], True)
                     if result:
                         self._addColumn(result)
@@ -303,57 +309,55 @@ class MetadataTable(object):
                         raise ValueError('Column creation failed for \'' + column + '\' however the column does exist in the column dictionary')
                 # Column not in dictionary - was added by user. Capture this field and store as metadata
                 else:
+                    self._log.append('Column does not exist in dictionary. Assuming user-defined column.')
                     result = column_factory.createColumn(column, 'text', '4000', False)
                     if result:
                         self._addColumn(result)
             except Exception as err:
-                raise err
+                return self._log
 
         # Read the column values
         try:
             for row in reader:
+                self._log.append('Reading header row:')
+                self._log.append(str(row))
                 # Skip any rows starting with white space
                 if len(row) == 0:
                     print 'Skipping row due to zero lengh'
                     print str(row)
                     continue
                 if row[0].startswith('\t') or row[0].startswith(' '):
-                    print 'Skipping row due to leading white space'
-                    print str(row)
+                    self._log.append('Skipping row due to leading white space')
                     continue
 
                 # If a row is incomplete, probably means end of file whitespace
                 if len(row) < len(self._columns):
-                    print 'Skipping row due to insufficient number of columns'
-                    print str(row)
+                    self._log.append('Skipping row due to insufficient number of columns')
                     continue
 
                 # There is data but not enough to fill the columns - pad out data
                 if len(row) > 1 and len(row) < len(self._columns):
+                    self._log.append('Row contains data but insufficient number of columns. Assuming blanks for remaining column values.')
                     i = 0
                     while i < len(self._columns) - len(row):
                         row.append('\t')
                         i += 1
-                
-                print len(row)
                 
                 i = 0
                 for column in row:
                     # Some files contain extra whitespace characters beyond the bounds of the last column.
                     # Skip over these values.
                     if (i > len(self._columns) - 1):
+                        self._log.append('Extra whitespace found beyond column count boundary. Ignoring...')
                         continue
                     
                     # Add the current value to the appropriate metadata table column
+                    self._log.append('Adding new column to metadata table: "%s"' % column.strip())
                     self._columns[i]._addValue(column.strip(), data_access)
                     i += 1
         except Exception, e:
-            msg = 'Error adding data to column %s. Maximum column index for this metadata table is %s. The error was: \n<p/>%s' % (str(i), str(len(self._columns) - 1), str(e))
-            print msg
-            raise Exception(msg)
-
-        #self._printTable()
-        #self._printInvalidRows()
+            self._log.append('Error adding data to column %s. Maximum column index for this metadata table is %s. The error was: \n<p/>%s' % (str(i), str(len(self._columns) - 1), str(e)))
+            return self._log
 
     def _addColumn(self, column):
         self._columns.append(column)
@@ -363,114 +367,105 @@ class MetadataTable(object):
         for row in self._invalid_rows:
             print row
 
-    """ depreciated
-    def _printTable(self):
-        print '\n\n'
-        column_count = len(self._columns)
-        row_count = len(self._columns[0].values)
-
-        x = 0
-
-        while x < column_count:
-            print self._columns[x].column_name + '\t',
-            x += 1
-
-        y = 0
-        
-        while y < row_count:
-            x = 0
-            print '\n',
-            while x < column_count:
-                for column in self._columns:
-                    print str(column.values[y]) + '\t',
-                    x += 1
-            y += 1 
-            
-        print '\n\n'
-    """
-
     def printHTMLTable(self):
-        # Determine the type of file we're dealing with
-        file_type = ''
-        if os.path.basename(self._metadataFile).startswith('study'):
-            file_type = 'study'
-        elif os.path.basename(self._metadataFile).startswith('sample'):
-            file_type = 'sample'
-        elif os.path.basename(self._metadataFile).startswith('prep'):
-            file_type = 'prep'
-        else:
-            return
+        # Log which function we're in
+        self._log.append('Entering printHTMLTable()...')
         
-        html_table = '<table class="metadata_table">'
-        
-        column_count = len(self._columns)
-        row_count = len(self._columns[0].values)
-
-        # Print the column headers
-        x = 0
-        while x < column_count:
-            current_column = self._columns[x]
-            if current_column._in_dictionary:
-                html_table += '<th class="meta_th">' + self._columns[x].column_name + '</th>\n'
-            else:
-                html_table += '<th class="meta_th" style="background:#00FFFF">' + self._columns[x].column_name + '</th>\n'
-            x += 1
-        
-        #####################################
-        # Print the table rows
-        #####################################
-        
-        # Max length for text columns
-        max_text_length = 50
-        
-        # Count of errors on page
+        # Return variables
+        html_table = ''
         error_count = 0
-        
-        # Index for rows
-        y = 0
-        
-        while y < row_count:
+
+        try:
+            # Determine the type of file we're dealing with
+            file_type = ''
+            base_name = os.path.basename(self._metadataFile)
+            if 'study_template' in base_name:
+                file_type = 'study'
+            elif 'sample_template' in base_name:
+                file_type = 'sample'
+            elif 'prep_template' in base_name:
+                file_type = 'prep'
+            else:
+                self._log.append('Could not determine type of file: "%s". Exiting.' % self._metadataFile)
+                return None, 0, log
+            
+            self._log.append('File type is "%s"' % file_type)
+            
+            html_table = '<table class="metadata_table">'
+            
+            column_count = len(self._columns)
+            row_count = len(self._columns[0].values)
+    
+            # Print the column headers
+            self._log.append('Print column headers...')
             x = 0
-            html_table += '<tr>\n'
             while x < column_count:
-                for column in self._columns:
-
-                    # Create a unique column name for processing later
-                    unique_column_name = file_type + ':' + str(y) + ':' + str(x) + ':' + column.column_name
-
-                    # This is the output value for the HTML page. It may be truncated for display if the
-                    # text is very long.
-                    value_output = str(column.values[y][0])
-
-                    # This will always hold the full value of the field. Used for submission to database.
-                    actual_value = str(column.values[y][0])
+                current_column = self._columns[x]
+                self._log.append('Current column is "%s"' % current_column)
+                if current_column._in_dictionary:
+                    html_table += '<th class="meta_th">' + self._columns[x].column_name + '</th>\n'
+                    self._log.append('Column is in dictionary')
+                else:
+                    html_table += '<th class="meta_th" style="background:#00FFFF">' + self._columns[x].column_name + '</th>\n'
+                    self._log.append('Column is not in dictionary - assuming user-defined column.')
+                x += 1
+            
+            #####################################
+            # Print the table rows
+            #####################################
+            
+            # Max length for text columns
+            max_text_length = 50
+            
+            # Index for rows
+            y = 0
+            
+            while y < row_count:
+                x = 0
+                html_table += '<tr>\n'
+                while x < column_count:
+                    for column in self._columns:
+    
+                        # Create a unique column name for processing later
+                        unique_column_name = file_type + ':' + str(y) + ':' + str(x) + ':' + column.column_name
+    
+                        # This is the output value for the HTML page. It may be truncated for display if the
+                        # text is very long.
+                        value_output = str(column.values[y][0])
+    
+                        # This will always hold the full value of the field. Used for submission to database.
+                        actual_value = str(column.values[y][0])
+                        
+                        #For fields that are valid:
+                        if column.values[y][1] == 'good':
+                            # If this is a text column, truncate the length of the string to keep the display reasonable
+                            if type(column) == TextColumn:
+                                if len(value_output) > max_text_length:
+                                    value_output = value_output[:max_text_length] + '...'
+                                
+                            hidden_field_text = '<input type=\"hidden\" id=\"%s\" name=\"%s\" value=\"%s\">\n' % (unique_column_name, unique_column_name, actual_value)
+                            cell_color = '#FFFFFF'
+                            html_table += '<td style=\"background-color:%s;\">%s%s</td>\n' % (cell_color, hidden_field_text, value_output)
+                        
+                        # For fields that are not valid
+                        else:
+                            error_count += 1
+                            cell_color = '#EEEEFF'
+                            html_table += '<td style="background-color:#DDDDDD;"><input style="background-color:%s;" type="text" id="%s" name="%s" value="%s" %s> <br/> \
+                                <a href="" onclick="replaceWithCurrent(\'%s\');return false;"><div style="font-size:11px">replace all</div></a></td>\n' \
+                                % (cell_color, unique_column_name, unique_column_name, actual_value, column.writeJSValidation(), unique_column_name)
+                        x += 1
+                        
+                html_table +='</tr>\n'
+                y += 1 
+            
+            html_table += '</table>\n'
                     
-                    #For fields that are valid:
-                    if column.values[y][1] == 'good':
-                        # If this is a text column, truncate the length of the string to keep the display reasonable
-                        if type(column) == TextColumn:
-                            if len(value_output) > max_text_length:
-                                value_output = value_output[:max_text_length] + '...'
-                            
-                        hidden_field_text = '<input type=\"hidden\" id=\"%s\" name=\"%s\" value=\"%s\">\n' % (unique_column_name, unique_column_name, actual_value)
-                        cell_color = '#FFFFFF'
-                        html_table += '<td style=\"background-color:%s;\">%s%s</td>\n' % (cell_color, hidden_field_text, value_output)
-                    
-                    # For fields that are not valid
-                    else:
-                        error_count += 1
-                        cell_color = '#EEEEFF'
-                        html_table += '<td style="background-color:#DDDDDD;"><input style="background-color:%s;" type="text" id="%s" name="%s" value="%s" %s> <br/> \
-                            <a href="" onclick="replaceWithCurrent(\'%s\');return false;"><div style="font-size:11px">replace all</div></a></td>\n' \
-                            % (cell_color, unique_column_name, unique_column_name, actual_value, column.writeJSValidation(), unique_column_name)
-                    x += 1
-                    
-            html_table +='</tr>\n'
-            y += 1 
+            # Form variable to hold count of errors on page. Determines when "sumbit" will be enabled.
+            #html_table += '<input type="hidden" name="%s_error_count" value="%s"\n' % (file_type, error_count)
+            
+            return html_table, error_count, self._log
         
-        html_table += '</table>\n'
-                
-        # Form variable to hold count of errors on page. Determines when "sumbit" will be enabled.
-        #html_table += '<input type="hidden" name="%s_error_count" value="%s"\n' % (file_type, error_count)
-        
-        return html_table, error_count
+        except Exception, e:            
+            self._log.append('Error caught in printHTMLTable: %s' % str(e))
+            return html_table, error_count, self._log
