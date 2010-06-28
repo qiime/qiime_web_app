a28fa99ff5afc9e03b571439661823a01acf4ca7
jistombaugh <jistombaugh@aa835298-08e1-4b45-9256-153aff0879ff>
2010-06-28 19:53:13 +0000

    adding and updating scripts to repository
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@254 aa835298-08e1-4b45-9256-153aff0879ff

---
+++ python_code/scripts/submit_sff_and_metadata_to_db.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# File created on 11 Jun 2010
+from __future__ import division
+
+__author__ = "Jesse Stombaugh"
+__copyright__ = "Copyright 2010, The QIIME WebApp project"
+__credits__ = ["Jesse Stombaugh"]
+__license__ = "GPL"
+__version__ = "dev"
+__maintainer__ = "Jesse Stombaugh"
+__email__ = "jesse.stombaugh@colorado.edu"
+__status__ = "Development"
+ 
+from qiime.util import parse_command_line_parameters, get_options_lookup
+from optparse import make_option
+from os import makedirs
+from qiime.util import load_qiime_config
+from qiime.util import load_qiime_config
+from process_sff_and_metadata_workflow import submit_processed_data_to_db
+
+
+qiime_config = load_qiime_config()
+options_lookup = get_options_lookup()
+
+script_info = {}
+script_info['brief_description'] = "Submit processed SFF and metadata through picking OTUs into the Oracle DB"
+script_info['script_description'] = """\
+This script takes an processed sff fasta file and performs the \
+following steps:
+
+    1) 
+    2) 
+    3) 
+    4) 
+"""
+script_info['script_usage'] = [("Example:","This is an example of a basic use case",
+"%prog -i 454_Reads.fna")]
+script_info['output_description']= "There is no output from the script is puts the processed data into the Oracle DB."
+script_info['required_options'] = [\
+    make_option('-i','--processed_fasta_fnames',help='This is the processed fasta filepath(s) from process_sff.py'),\
+]
+script_info['optional_options'] = [\
+]
+script_info['version'] = __version__
+
+def main():
+    option_parser, opts, args =\
+       parse_command_line_parameters(**script_info)
+    
+    fasta_files=opts.processed_fasta_fnames
+
+
+    submit_processed_data_to_db(fasta_files=fasta_files)
+
+if __name__ == "__main__":
+    main()
