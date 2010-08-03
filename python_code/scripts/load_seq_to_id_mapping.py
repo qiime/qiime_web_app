a484d1565f14c2e9b50fc5b0e10fae231b4d7c63
jistombaugh <jistombaugh@aa835298-08e1-4b45-9256-153aff0879ff>
2010-08-03 19:20:32 +0000

    Added the ability to map a sequence to it's source.
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@296 aa835298-08e1-4b45-9256-153aff0879ff

---
+++ python_code/scripts/load_seq_to_id_mapping.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# File created on 03 Aug 2010
+from __future__ import division
+
+__author__ = "Jesse Stombaugh"
+__copyright__ = "QIIME-webdev"
+__credits__ = ["Jesse Stombaugh"]
+__license__ = "GPL"
+__version__ = "1.1.0-dev"
+__maintainer__ = "Jesse Stombaugh"
+__email__ = "jesse.stombaugh@colorado.edu"
+__status__ = "Development"
+ 
+
+from optparse import make_option
+from qiime.util import parse_command_line_parameters, get_options_lookup
+from load_tab_file import input_set_generator
+from cogent.parse.fasta import MinimalFastaParser
+from os.path import split
+import cx_Oracle
+options_lookup = get_options_lookup()
+
+script_info = {}
+script_info['brief_description'] = ""
+script_info['script_description'] = ""
+script_info['script_usage'] = [("","","")]
+script_info['output_description']= ""
+script_info['required_options'] = [\
+make_option('-i','--input_file',help='the input fasta file, where the sequence ids are the prokmsa_ids')
+ # Example required option
+ #make_option('-i','--input_dir',help='the input directory'),\
+ #options_lookup['input_fasta']
+]
+script_info['optional_options'] = [\
+ # Example optional option
+ #make_option('-o','--output_dir',help='the output directory [default: %default]'),\
+]
+script_info['version'] = __version__
+
+
+
+def main():
+    option_parser, opts, args =\
+        parse_command_line_parameters(**script_info)
+
+    fasta_file = dict(MinimalFastaParser(open(opts.input_file,'U')))
+    fname=split(opts.input_file)[-1].split('_')
+    ref_dataset=fname[0]
+    if ref_dataset=='gg':
+        reference_dataset='GREENGENES_REFERENCE'
+    threshold=fname[1]
+    print threshold
+    con = cx_Oracle.connect(user='SFF',
+                           password='454SFF454',
+                           dsn='microbiome1.colorado.edu:1523/microbe')
+    
+    prokmsas=[]
+    for prok_id in fasta_file:
+        prokmsas.append('%s\t%s\t%s' % (str(prok_id),str(threshold),
+                                            reference_dataset))
+        
+    cur = con.cursor()
+
+    data_types=['i','i','s']
+    for input_set in input_set_generator(prokmsas, cur,data_types):
+        valid=data_access.loadSeqToSourceMap(True,input_set)
+        if not valid:
+            raise ValueError, 'Error: Unable to load Sequence to Source Map!'
+
+
+    
+
+if __name__ == "__main__":
+    main()
