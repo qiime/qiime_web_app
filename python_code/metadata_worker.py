0be14ff27b8bd3443dab98154da1f4f6c0b73aea
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2010-08-18 01:05:48 +0000

    correcting host_key_value issue
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@318 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/metadata_worker.py
+++ python_code/metadata_worker.py
@@ -49,42 +49,57 @@ class MetadataWorkerThread(threading.Thread):
         da = QiimeDataAccess()
         
         for item in self.item_list:
+            try:
             
-            # Reset the key_field
-            key_field = None
+                # Reset the key_field
+                key_field = None
             
-            # Put the parts into more meaningful variables
-            parts = item.split(':')
-            field_type = parts[0]
-            row_num = parts[1]
-            field_name = parts[3]
-            field_value = self.form[item]
+                # Put the parts into more meaningful variables
+                parts = item.split(':')
+                field_type = parts[0]
+                row_num = parts[1]
+                field_name = parts[3]
+                field_value = self.form[item]
             
-            # Figure out what the key field is going to be
-            if field_type == 'sample':
-                key_field = self.sample_key_fields[row_num]
-            elif field_type == 'prep':
-                key_field = self.prep_key_fields[row_num]
-            elif field_type == 'study':
-                key_field = self.study_name
+                # Figure out what the key field is going to be
+                if field_type == 'sample':
+                    key_field = self.sample_key_fields[row_num]
+                elif field_type == 'prep':
+                    key_field = self.prep_key_fields[row_num]
+                elif field_type == 'study':
+                    key_field = self.study_name
             
-            if len(self.host_key_fields) > 0:
-                host_key_field = self.host_key_fields[row_num]
-            else:
-                host_key_field = None
+                if len(self.host_key_fields) > 0 and field_type == 'sample':
+                    try:
+                        host_key_field = self.host_key_fields[row_num]
+                    except:
+                        # Do nothing if not found
+                        pass
+                else:
+                    host_key_field = None
             
-            # Just in case...
-            if key_field == None:
-                continue
+                # Just in case...
+                if key_field == None:
+                    continue
             
-            # For oracle, clean up single quotes
-            field_value = field_value.replace('\'', '\'\'')
+                # For oracle, clean up single quotes
+                field_value = field_value.replace('\'', '\'\'')
             
-            lock = Lock()
-            
+                lock = Lock()
+            except Exception, e:
+                self.req.write('<p/>')
+                self.req.write('Error was: ' + str(e) + '<br>')
+                self.req.write('Item is: ' + str(item) + '<br>')
+                self.req.write('Key Field is: ' + key_field + '<br>')
+                self.req.write('Host Key Field is: ' + host_key_field + '<br>')
+                self.req.write('Field name is: ' + field_name + '<br>')
+                self.req.write('Field value is: ' + field_value + '<br>')
+                self.req.write('<p>')
+
             try:
                 lock.acquire()
                 #result = self.data_access.writeMetadataValue(field_type, key_field, field_name, field_value, self.study_id, host_key_field)
+                #self.req.write('<br/><br/>' + str(item) + ' ------ ' + field_value + ' ------ ' + field_type + '<br/><br/>')
                 result = da.writeMetadataValue(field_type, key_field, field_name, field_value, self.study_id, host_key_field)
                 if result:
                     self.req.write('<br/><br/>' + result + '<br/><br/>')
