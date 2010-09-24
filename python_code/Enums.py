301b3fa7306541eff1588d61fb29c9110c5932b2
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2010-09-24 16:44:07 +0000

    adding Enums.py
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@359 aa835298-08e1-4b45-9256-153aff0879ff

---
+++ python_code/Enums.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+FieldGrouping.py
+
+Created by Doug Wendel on 2010-09-24.
+An "enum" for listing out the various field groupings rathern than referring
+to them by an index.
+"""
+
+class FieldGrouping:
+    sra_submission_level = -8
+    sra_study_level = -7
+    sra_sample_level = -6
+    sra_experiment_level = -5
+    study_level = -3
+    sample_level = -2
+    prep_level = -1
