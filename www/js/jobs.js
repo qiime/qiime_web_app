48e9a385c01089a62923e07def950db4945821d1
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2010-10-20 21:37:57 +0000

    adding dymanic job polling code
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@471 aa835298-08e1-4b45-9256-153aff0879ff

---
+++ www/js/jobs.js
@@ -0,0 +1,61 @@
+/*
+
+__author__ = "Doug Wendel"
+__copyright__ = "Copyright 2010, Qiime Web Analysis"
+__credits__ = ["Doug Wendel"]
+__license__ = "GPL"
+__version__ = "1.0.0.dev"
+__maintainer__ = ["Doug Wendel"]
+__email__ = "wendel@colorado.edu"
+__status__ = "Production"
+
+*/
+
+var xmlhttp;
+
+function testMe()
+{
+    alert('just testing');
+}
+
+// Need to factor this funciton out into its own file
+function GetXmlHttpObject()
+{
+    if (window.XMLHttpRequest)
+    {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        return new XMLHttpRequest();
+    }
+
+    if (window.ActiveXObject)
+    {
+        // code for IE6, IE5
+        return new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return null;
+}
+
+function checkJobStatus()
+{
+    // check if browser can perform xmlhttp
+    xmlhttp = GetXmlHttpObject()
+    if (xmlhttp==null)
+    {
+        alert ("Your browser does not support XML HTTP Request");
+        return;
+    }
+    
+    var url = "check_job_status.psp";
+
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4)
+        {
+            document.getElementById("job_status_div").innerHTML = xmlhttp.responseText;
+        }
+    }
+    
+    // perform a GET 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(null);
+ }
