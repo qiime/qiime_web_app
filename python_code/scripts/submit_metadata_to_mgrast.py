88516b876b83a04990fbe2528e345412a522219b
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2011-03-10 19:06:03 +0000

    adding mgrast export code
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@687 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/scripts/submit_metadata_to_mgrast.py
+++ python_code/scripts/submit_metadata_to_mgrast.py
@@ -14,21 +14,15 @@ __status__ = "Development"
 
 from optparse import make_option
 from qiime.util import parse_command_line_parameters, get_options_lookup
-from qiime.submit_to_mgrast import parse_and_submit_params
-from os import mkdir
-from os.path import exists
+from run_submit_metadata_to_mgrast import submit_metadata_for_study
 options_lookup = get_options_lookup()
 
 script_info = {}
 script_info['brief_description'] = "This script submits metadata to MG-RAST based on a study_id"
 script_info['script_description'] = "This script takes a study_id and an MG-RAST web service key and performs metadata submission to the MG-RAST system."
-script_info['script_usage'] = [("Example","The user can submit a post-split-library FASTA file, which will be loaded and processed into MG-RAST under the users account ('-w') and project ('-p'), as follows:","submit_metadata_to_mgrast.py -i split_lib_seqs.fna -w user_mgrast_auth_key -p qiime_test_dataset -o ./output_dir")]
-script_info['output_description']= "The resulting directory will contain all of the sample-separated FASTA files, along with a log html file, which informs the user of the jobs started on MG-RAST"
+script_info['script_usage'] = [("Example","python submit_data_to_mgrast.py -s 12345")]
 script_info['required_options'] = [\
- options_lookup['fasta_as_primary_input'],
- make_option('-w','--web_key_auth',help='the web services authorization key from MG-RAST'),
- make_option('-p','--project_id',help='the title to be used for the project'),
- options_lookup['output_dir'],
+ make_option('-s','--study_id',help='The study id to be exported')
 ]
 script_info['optional_options'] = [\
 ]
@@ -40,25 +34,10 @@ def main():
        parse_command_line_parameters(**script_info)
 
     #define the variables
-    project_id = opts.project_id
-    files = opts.input_fasta_fp
-    key = opts.web_key_auth
-    
-    #create directory path
-    if opts.output_dir:
-        if exists(opts.output_dir):
-            output_dir=opts.output_dir
-        else:
-            try:
-                mkdir(opts.output_dir)
-                output_dir=opts.output_dir
-            except OSError:
-                pass
-    else:
-        dir_path='./'
+    study_id = opts.study_id
     
     #call the main function
-    log_info=parse_and_submit_params(key, project_id, files,output_dir)
+    result = submit_metadata_for_study(study_id)
     
         
 if __name__ == "__main__":
