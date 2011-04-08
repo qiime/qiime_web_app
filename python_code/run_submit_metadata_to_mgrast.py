5f29255f82410dd2a75b0d3bc386d8a93bdb5e5c
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2011-04-08 16:38:45 +0000

    lotsastuff
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@769 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/run_submit_metadata_to_mgrast.py
+++ python_code/run_submit_metadata_to_mgrast.py
@@ -115,10 +115,10 @@ def submit_metadata_for_study(key, study_id, web_app_user_id, debug = False):
     # Get a copy of data access
     data_access = data_access_factory(ServerConfig.data_access_type)
 
-    study_cgi_path = '/~wilke/service/%s/study' % key
-    sample_cgi_path = '/~wilke/service/%s/sample' % key
-    prep_cgi_path = '/~wilke/service/%s/preparation' % key
-    sequence_cgi_path = '/~wilke/service/%s/reads' % key
+    study_cgi_path = '/service/%s/study' % key
+    sample_cgi_path = '/service/%s/sample' % key
+    prep_cgi_path = '/service/%s/preparation' % key
+    sequence_cgi_path = '/service/%s/reads' % key
     
     study_file_path = '/tmp/mgrast_study_metadata_%s.xml' % study_id
     sample_file_path = '/tmp/mgrast_sample_metadata_%s.xml' % study_id
@@ -129,7 +129,7 @@ def submit_metadata_for_study(key, study_id, web_app_user_id, debug = False):
     # Attempt to reslve the MG-RAST host
     # host = '140.221.76.10'
     #host = resolve_host(study_cgi_path)
-    host = 'dunkirk.mcs.anl.gov'
+    host = 'metagenomics.anl.gov'
     if host is None:
         print 'Could not resolve host. Aborting.'
         return
