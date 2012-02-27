f440db51c3b20a5d46a73f54f86e93ea499d07da
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2012-02-27 21:32:19 +0000

    adding
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@1190 aa835298-08e1-4b45-9256-153aff0879ff

---
+++ www/js/utils.js
@@ -0,0 +1,20 @@
+// Numeric only control handler
+jQuery.fn.ForceNumericOnly =
+function()
+{
+    return this.each(function()
+    {
+        $(this).keydown(function(e)
+        {
+            var key = e.charCode || e.keyCode || 0;
+            // allow backspace, tab, delete, arrows, numbers and keypad numbers ONLY
+            return (
+                key == 8 || 
+                key == 9 ||
+                key == 46 ||
+                (key >= 37 && key <= 40) ||
+                (key >= 48 && key <= 57) ||
+                (key >= 96 && key <= 105));
+        });
+    });
+};
