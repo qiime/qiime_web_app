3e3047186943e0efd65f4a2252e1e321057913ce
jistombaugh <jistombaugh@aa835298-08e1-4b45-9256-153aff0879ff>
2012-03-02 00:21:32 +0000

    updated to use sparse representation from DB
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@1196 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/generate_mapping_and_otu_table.py
+++ python_code/generate_mapping_and_otu_table.py
@@ -381,38 +381,35 @@ def write_mapping_and_otu_table(data_access, table_col_value, fs_fp, web_fp,
     t1 = time()
     
     
-    #con = data_access.getSFFDatabaseConnection()
-    #cur = con.cursor()
+    # Get the OTU-Ids and counts for each sample
     query=[]
-    sample_counts={}
-    otus=[]
-    
+    data = []
     for i,sample_name1 in enumerate(samples_list):
-        sample_counts[sample_name1]={}
-        user_data=data_access.getOTUTable(True,sample_name1,'UCLUST_REF',97, 'GREENGENES_REFERENCE',97)
+        user_data=data_access.getOTUTable(True,sample_name1,'UCLUST_REF',97, 
+                                          'GREENGENES_REFERENCE',97)
         for row in user_data:
-            if row[0] not in otus:
-                otus.append(str(row[0]))
-            if sample_counts[sample_name1].has_key(str(row[0])):
-                raise ValueError, 'Duplicate prokmsa ids! - %s ' % sample_name1
             try:
-                sample_counts[sample_name1][str(row[0])]=row[1]
+                data.append((str(row[0]), sample_name1, row[1]))
             except:
                 continue
 
-    otu_table=zeros((len(otus),len(samples_list)),dtype=int)
-    
-    for i,sample in enumerate(samples_list):
-        for j, otu in enumerate(otus):
-            if sample_counts.has_key(sample):
-                if sample_counts[sample].has_key(otu):
-                    otu_table[j][i]=sample_counts[sample][otu]
-             
+    # sort observations and sample-ids
+    sample_order = sorted(set([r[1] for r in data]))
+    obs_order = sorted(set([r[0] for r in data]))
 
+    # convert into dictionary
+    sample_idx = dict([(s,i) for i,s in enumerate(sample_order)])
+    obs_idx = dict([(o,i) for i,o in enumerate(obs_order)])
+
+    # prepare data for submission to table_factory
+    table_data = {}
+    for obs,sample,value in data:
+        table_data[obs_idx[obs], sample_idx[sample]] = value
+    
+    # Get Taxonomy information for each observation
     taxonomy=[]
-    for i in otus:
+    for i in obs_order:
         tax_str=data_access.getGGTaxonomy(True,i,tax_name+'_tax_string')
-        
         if not tax_str:
             taxonomy.append('')
         else:
@@ -422,7 +419,8 @@ def write_mapping_and_otu_table(data_access, table_col_value, fs_fp, web_fp,
     otu_table_filepath=os.path.join(otu_table_file_dir, otu_table_fname)
     otu_table_filepath_db=os.path.join(otu_table_file_dir_db, otu_table_fname)
 
-    otu_biom=table_factory(otu_table,samples_list,otus,
+    # create the biom object
+    otu_biom=table_factory(table_data,sample_order,obs_order,
                            sample_metadata=None,
                            observation_metadata=taxonomy,
                            table_id=None,
