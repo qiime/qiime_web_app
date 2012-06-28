f1ee18e29efec4596adc34e32419c69537589b69
wendel <wendel@aa835298-08e1-4b45-9256-153aff0879ff>
2012-06-28 12:30:12 +0000

    fixing FASTA submission
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@1656 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/data_access/qiime_data_access.py
+++ python_code/data_access/qiime_data_access.py
@@ -386,7 +386,7 @@ class QiimeDataAccess(object):
 			study_info['funding'] = row[37]
 			study_info['includes_timeseries'] = row[38]
 			study_info['sample_count'] = row[39]
-			study_info['ebi_accession'] = row[40]
+			study_info['ebi_study_accession'] = row[40]
 		return study_info
 
 	def saveTimeseriesData(self, study_id, timeseries_file, req):
