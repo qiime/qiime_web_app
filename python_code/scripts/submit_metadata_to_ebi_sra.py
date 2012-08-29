8325d7e4f4a8d7ca0dabfb883bf63a205477d992
jistombaugh <jistombaugh@aa835298-08e1-4b45-9256-153aff0879ff>
2012-08-29 21:32:43 +0000

    automation
    git-svn-id: svn://svn.microbio.me/QIIME-webdev/trunk/qiime_web_app@2277 aa835298-08e1-4b45-9256-153aff0879ff

--- python_code/scripts/submit_metadata_to_ebi_sra.py
+++ python_code/scripts/submit_metadata_to_ebi_sra.py
@@ -15,8 +15,12 @@ __status__ = "Development"
 from optparse import make_option
 from qiime.util import parse_command_line_parameters
 from live_ebi_sra_rest_services import LiveEBISRARestServices
-
 from os.path import join
+from os import chmod
+from data_access_connections import data_access_factory
+from enums import ServerConfig
+from subprocess import Popen, PIPE, STDOUT
+from sys import stdout
 
 script_info = {}
 script_info['brief_description'] = "This script submits metadata to the EBI SRA based on a study_id"
@@ -40,6 +44,7 @@ def main():
     study_dir = root_dir + '/study_{0}'.format(str(study_id))
     ebi_export_log_fp = join(study_dir, 'ebi_export_log.txt')
     curl_output_fp = join(study_dir, 'curl_output.xml')
+    curl_command_fp = join(study_dir, 'ebi_curl_command_{0}.sh'.format(study_id))
 
     # Get the live EBI function reference
     live = LiveEBISRARestServices(study_id, web_app_user_id, root_dir, debug)
@@ -47,6 +52,7 @@ def main():
     
     # Send the sequence files first - required for metadata to validate. If files have already been
     # sent then skip this step.
+    data_access = data_access_factory(ServerConfig.data_access_type)
     statement = 'select ebi_files_sent from study where study_id = {0}'.format(study_id)
     ebi_files_sent = data_access.dynamicMetadataSelect(statement).fetchone()[0]
     if ebi_files_sent != 1:
@@ -54,9 +60,35 @@ def main():
     
     # Submit the metadata with the VALIDATE attribute first. Performs no other actions
     # on EBI server other than ensuring that XML validates. 
+
+    # Generate metadata and curl command
     live.generate_metadata_files(debug = True, action_type = 'VALIDATE')
-    curl_output = open(curl_output_fp, 'r')
+    chmod(curl_command_fp, 0755)
+
+    # Call curl command
+    curl_output = open(curl_output_fp, 'w')
+    run_list = ['{0}'.format(curl_command_fp)]
+    proc = Popen(run_list, shell=True, universal_newlines=True, stdout=PIPE)
+    complete = False
+    while True:
+        out = proc.stdout.read(1)
+        if out == '' and proc.poll() != None:
+            break
+        if out != '':
+            curl_output.write(out)
     curl_output.close()
+
+    # Read the output file
+    curl_output = open(curl_output_fp, 'r')
+    curl_result = curl_output.read()
+    curl_output.close()
+    if 'success="true"' in curl_result:
+        print 'VALID'
+    elif 'success="false"' in curl_result:
+        print 'INVALID'
+    else:
+        print 'Unknown result'
+
     
     # If success, then submit with 'ADD' or 'MODIFY'
     # Read output
