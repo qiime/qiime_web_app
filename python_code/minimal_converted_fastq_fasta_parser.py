b393c2338d007170b4d06c21626b7f43647a1dd9
Doug Wendel <douginator2000@gmail.com>
2013-01-08 13:06:03 -0700

    fixes for parsing post split_libraries fastq qual data

---
+++ python_code/minimal_converted_fastq_fasta_parser.py
@@ -0,0 +1,20 @@
+def MinimalConvertedFastqFastaParser(infile):
+    """ Internal function for yielding 2 lines of file at a time 
+
+    This parser handles the case in which split_libraries has already
+    produced a FASTA formatted file from illumina data. Quality scores
+    in files produced this way can begin with the normally reserved 
+    character of ">". This parser simply reads pairs of rows and assumes
+    that the first is the label and second is the sequence.
+    """
+    count = 0
+    for line in infile:
+        strip(line)
+        # First row of pair is always the label
+        if count == 0:
+            label = line[1:]
+            count += 1
+        elif count == 1:
+            seq = line
+            count = 0
+            yield label, seq
