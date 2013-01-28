c55a1e8037c2d2d0b0806f0a8c2287e0a51e4cbe
Doug Wendel <douginator2000@gmail.com>
2013-01-28 13:12:51 -0700

    consent form

--- www/consent/js/american_gut.js
+++ www/consent/js/american_gut.js
@@ -1,4 +1,6 @@
-$(function(){
+
+$(function()
+{
     consent_info.parent_1_name.disabled = true
     consent_info.parent_2_name.disabled = true
     consent_info.parent_1_code.disabled = true
@@ -6,25 +8,31 @@ $(function(){
     consent_info.deceased_parent.disabled = true
 });
 
-$(function(){
-    $('input.submit').click(function(){
-        for(var i = 0; i < document.consent_info.length; i++)
-            document.consent_info[i].className = document.consent_info[i].className.replace
-      (/(?:^|\s)highlight(?!\S)/ , '');
-      document.getElementById("consent").className = document.getElementById("consent").className.replace
-      (/(?:^|\s)highlight(?!\S)/ , '');
+$(function()
+{
+    $('#submit_form').click(function()
+    {
+        for(var i = 0; i < document.consent_info.length; i++) 
+        {
+            document.consent_info[i].className = document.consent_info[i].className.replace(/(?:^|\s)highlight(?!\S)/ , '');
+        }
+
+      	document.getElementById("consent").className = document.getElementById("consent").className.replace(/(?:^|\s)highlight(?!\S)/ , '');
       
         var valid = true;
+        
         if(!document.consent_info.consent.checked)
         {
             document.getElementById("consent").className += " highlight";
             valid = false;
         }
+
         if(document.consent_info.participant_name.value == "")
         {
             document.consent_info.participant_name.className += " highlight";
             valid = false;
         }
+
         if(document.consent_info.contact_code.value == "")
         {
             document.consent_info.contact_code.className += " highlight";
@@ -55,16 +63,23 @@ $(function(){
             }
         }
         
-        if(!valid)
+        if(!valid) 
+    	{
+    	    //alert($('#consent_info').submit());
             return;
-        else
-            alert($('#consent_info').submit());
+    	}
+        else 
+    	{
+            //alert($('#consent_info').submit());
             $('#consent_info').submit();
+    	}
     });
 });
 
-$(function(){
-    $('input.is_7_to_13').click(function(){
+$(function()
+{
+    $('input.is_7_to_13').click(function()
+    {
         if(document.consent_info.is_7_to_13.checked)
         {
             consent_info.parent_1_name.disabled = false
