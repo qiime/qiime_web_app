d64fc5ffa52da93e1567be301d77104e0ff63a4e
Doug Wendel <douginator2000@gmail.com>
2013-02-15 11:27:13 -0700

    making sticky

--- python_code/utils/update_seq_otu_stats.py
+++ python_code/utils/update_seq_otu_stats.py
@@ -1,6 +1,7 @@
 from data_access_connections import data_access_factory
 from enums import ServerConfig
 from summarize_seqs_otu_hits import summarize_all_stats
+import traceback
 
 data_access = data_access_factory(ServerConfig.data_access_type)
 query = 'select distinct sa.study_id from sample sa inner join sff.analysis a on sa.study_id = a.study_id order by study_id desc'
@@ -25,5 +26,6 @@ for result in results:
                 str(sequence_prep_id), str(sequence_count), str(otu_count))
 
     except Exception, e:
-        print 'Could not process study_id: {0}. The error was: "{1}"'.format(study_id, str(e))
+        print 'Could not process study_id: {0}. The error was: "{1}"\n{2}'.format(study_id, str(e), traceback.print_exc())
+
 
