0bb7b877e779fefde3421994c23776ff275c8b68
Doug Wendel <douginator2000@gmail.com>
2013-02-15 11:05:45 -0700

    making sticky

--- python_code/utils/update_seq_otu_stats.py
+++ python_code/utils/update_seq_otu_stats.py
@@ -9,7 +9,19 @@ results = data_access.dynamicMetadataSelect(query)
 for result in results:
 	study_id = result[0]
 	try:
-		summarize_all_stats(study_id)
+		print 'Updating study {0}'.format(study_id)
+		mapping, seq_header_lines, otu_header_lines = summarize_all_stats(study_id)
+
+		for sample_name, sequence_count, otu_count, percent_assignment in mapping:
+        sequence_prep_id = sample_name.split('.')[-1]
+	
+		print '    sequence_prep_id: {0}, seq_count: {1}, otu_count: {2}, percent_assignment: {3}'.format(sequence_prep_id, sequence_count, otu_count, percent_assignment)
+        data_access.updateSeqOtuCounts(sequence_prep_id, sequence_count, otu_count, percent_assignment)
+	
+        if debug:
+    		print 'added to database: prep: {0}, seq_count: {1}, otu_count: {2}'.format(\
+    			str(sequence_prep_id), str(sequence_count), str(otu_count))
+    
 	except Exception, e:
 		print 'Could not process study_id: {0}. The error was: "{1}"'.format(study_id, str(e))
 
