d16f907cb5fd9bb7745945c0a8dc6969fa8c8534
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2013-03-15 10:06:57 -0600

    Remove dynamicMetadataUpdate function
    As we discussed on Skype, not a good idea.

--- python_code/data_access/ag_data_access.py
+++ python_code/data_access/ag_data_access.py
@@ -61,17 +61,6 @@ class AGDataAccess(object):
         con = self.getMetadataDatabaseConnection()
         return con.cursor().execute(query_string)
 
-    def dynamicMetadataUpdate(self, query_string):
-        """ """
-        query_string_parts = set(query_string.lower().split())
-        verboten = set(['select', 'insert', 'delete'])
-        intersection = query_string_parts.intersection(verboten)
-        if len(intersection) > 0:
-            raise Exception('Only select statements are allowed. Your query: %s' % query_string)
-        con = self.getMetadataDatabaseConnection()
-        con.cursor().execute(query_string)
-        con.cursor().execute('commit')
-
     #####################################
     # Users
     #####################################
