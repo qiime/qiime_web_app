516b53b9dbb80e87223a61c9ec78271f5f0e91e3
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2013-04-15 22:14:16 -0600

    Add tests for tab_delim_lines_to_table

--- python_code/tests/test_psp_utils.py
+++ python_code/tests/test_psp_utils.py
@@ -13,7 +13,8 @@ __status__ = "Production"
 
 
 from cogent.util.unit_test import TestCase, main
-from utils.psp_utils import format_submit_form_to_fusebox_string
+from utils.psp_utils import (format_submit_form_to_fusebox_string,
+    tab_delim_lines_to_table)
 
 class ToplevelTests(TestCase):
     def test_format_submit_form_to_fusebox_string(self):
@@ -36,6 +37,85 @@ class ToplevelTests(TestCase):
 
         self.assertEqual(format_submit_form_to_fusebox_string(
             page='portal.psp'), change_page_only)
+
+    def test_tab_delim_lines_to_table(self):
+        self.assertEqual(tab_delim_lines_to_table(histograms_lines), defaults)
+
+        self.assertEqual(tab_delim_lines_to_table(histograms_lines, border=2,
+            width='100%'), with_table_mods)
+
+histograms_lines = '''#comment line
+#another comment line
+Length	Count
+114.0	1830490
+124.0	1981629
+134.0	7168533
+144.0	86386729
+154.0	0
+--
+'''.splitlines()
+
+with_table_mods = '''<table width=100% border=2>
+<tr>
+<td>Length</td>
+<td>Count</td>
+</tr>
+<tr>
+<td>114.0</td>
+<td>1830490</td>
+</tr>
+<tr>
+<td>124.0</td>
+<td>1981629</td>
+</tr>
+<tr>
+<td>134.0</td>
+<td>7168533</td>
+</tr>
+<tr>
+<td>144.0</td>
+<td>86386729</td>
+</tr>
+<tr>
+<td>154.0</td>
+<td>0</td>
+</tr>
+<tr>
+<td>--</td>
+</tr>
+</table>
+'''
+
+defaults = '''<table >
+<tr>
+<td>Length</td>
+<td>Count</td>
+</tr>
+<tr>
+<td>114.0</td>
+<td>1830490</td>
+</tr>
+<tr>
+<td>124.0</td>
+<td>1981629</td>
+</tr>
+<tr>
+<td>134.0</td>
+<td>7168533</td>
+</tr>
+<tr>
+<td>144.0</td>
+<td>86386729</td>
+</tr>
+<tr>
+<td>154.0</td>
+<td>0</td>
+</tr>
+<tr>
+<td>--</td>
+</tr>
+</table>
+'''
 
 if __name__ == "__main__":
     main()
