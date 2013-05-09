cbc93214fe5eda6720ac856bf2c90a33c954538c
Doug Wendel <douginator2000@gmail.com>
2013-05-08 20:00:52 -0600

    updating stored procedure

--- database/procedures/ag_get_next_barcode.sql
+++ database/procedures/ag_get_next_barcode.sql
@@ -11,13 +11,11 @@ begin
                     select  barcode
                     from    ag_kit_barcodes
                     where   length(barcode) = 9
-                            and barcode not in ('000009999', '000008888')
                             and barcode not like '9%'
                     union
                     select  barcode
                     from    ag_handout_kits
                     where   length(barcode) = 9
-                            and barcode not in ('000009999', '000008888')
                             and barcode not like '9%'
                     order by barcode desc
                 )
