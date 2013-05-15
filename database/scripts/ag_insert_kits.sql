1a4207c33b1011edced2d843b6c3fa010c981b90
Doug Wendel <douginator2000@gmail.com>
2013-05-15 10:19:53 -0600

    making sticky

--- database/scripts/ag_insert_kits.sql
+++ database/scripts/ag_insert_kits.sql
@@ -11,6 +11,78 @@ select * from ag_kit_barcodes where ag_kit_id = 'D9F2A0F20C7AAF51E0408A800C5D0A2
 
 */
 
+select * from tmp_kits;
+
+-- remove decimal point
+select  '00000' || substr(barcode, 0, instr(barcode, '.') - 1) as barcode
+from    tmp_kits;
+
+update  tmp_kits
+set     barcode = '00000' || substr(barcode, 0, instr(barcode, '.') - 1);
+commit;
+
+-- CLEANUP OF NUMERIC VALUES for US ONLY zip codes
+update  tmp_kits
+set     zip = '0' || zip
+where   length(zip) = 4;
+
+update  tmp_kits
+set     zip = '00' || zip
+where   length(zip) = 3;
+
+update  tmp_kits
+set     zip = '000' || zip
+where   length(zip) = 2;
+
+update  tmp_kits
+set     zip = '0000' || zip
+where   length(zip) = 1;
+
+update  tmp_kits
+set     kit_password = '0' || kit_password
+where   length(kit_password) = 7;
+
+update  tmp_kits
+set     kit_password = '00' || kit_password
+where   length(kit_password) = 6;
+
+update  tmp_kits
+set     kit_password = '000' || kit_password
+where   length(kit_password) = 5;
+
+update  tmp_kits
+set     kit_password = '0000' || kit_password
+where   length(kit_password) = 4;
+
+update  tmp_kits
+set     kit_password = '00000' || kit_password
+where   length(kit_password) = 3;
+
+update  tmp_kits
+set     kit_password = '000000' || kit_password
+where   length(kit_password) = 2;
+
+update  tmp_kits
+set     kit_password = '0000000' || kit_password
+where   length(kit_password) = 1;
+
+update  tmp_kits
+set     kit_verification_code = '0' || kit_verification_code
+where   length(kit_verification_code) = 4;
+
+update  tmp_kits
+set     kit_verification_code = '00' || kit_verification_code
+where   length(kit_verification_code) = 3;
+
+update  tmp_kits
+set     kit_verification_code = '000' || kit_verification_code
+where   length(kit_verification_code) = 2;
+
+update  tmp_kits
+set     kit_verification_code = '0000' || kit_verification_code
+where   length(kit_verification_code) = 1;
+
+commit;
 
 select count(distinct email) from tmp_kits;
 select count(distinct kit_id) from tmp_kits;
@@ -44,7 +116,7 @@ begin
         
         merge into ag_login
         using dual
-        on (dual.dummy is not null and ag_login.email = r.email)
+        on (dual.dummy is not null and lower(ag_login.email) = lower(r.email))
         when not matched then 
             insert (email, name, address, city, state, zip, country) 
             values (r.email, r.name, r.address, r.city, r.state, r.zip, r.country);
@@ -71,13 +143,9 @@ begin
         on (dual.dummy is not null and barcode = r.barcode)
         when not matched then
             insert (ag_kit_id, barcode, sample_barcode_file, sample_barcode_file_md5)
-            values (ag_kit_id_, r.barcode, r.sample_barcode_file, '');
+            values (ag_kit_id_, r.barcode, r.barcode || '.jpg', '');
         
         --dbms_output.put_line(ag_kit_id_);
         
     end loop;
 end;
-
-
-
-
