c0caaebb1f628c2fbadfdc597a8b5a48e23e9087
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2013-07-02 14:30:59 -0600

    Add function can_send_mail
    takes no arguments, returns true if 'microbio.me' is in socket.hostname();
    returns false otherwise

--- python_code/utils/mail.py
+++ python_code/utils/mail.py
@@ -12,6 +12,9 @@ __maintainer__ = ["Yoshiki Vazquez-Baeza"]
 __email__ = "yoshiki89@gmail.com"
 __status__ = "Development"
 
+# used in the can_send_mail function
+from socket import gethostname
+
 # Import smtplib for the actual sending function
 import smtplib
 
@@ -19,6 +22,11 @@ import smtplib
 from email.mime.text import MIMEText
 
 FROM_EMAIL = 'info@americangut.org'
+
+def can_send_mail():
+    if 'microbio.me' in gethostname():
+        return True
+    return False
 
 def send_email(message, subject, recipient='americangut@gmail.com'):
     """Send an email from your local host """
