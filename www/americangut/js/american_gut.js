3693df3ba3290f486749b43739f9dc5a88dfde30
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2013-08-09 11:32:13 -0400

    Add enforcement for other sample sources

--- www/americangut/js/american_gut.js
+++ www/americangut/js/american_gut.js
@@ -581,7 +581,7 @@ function verifyAddSample() {
         document.add_sample.sample_time.className += " highlight";
         valid = false;
     }
-    if(document.add_sample.sample_site.selectedIndex == 0)
+    if((typeof(document.add_sample.sample_site) != 'undefined' && document.add_sample.sample_site.selectedIndex == 0) || (typeof(document.add_sample.environment_sampled) != 'undefined' && document.add_sample.environment_sampled.selectedIndex == 0))
     {
         document.getElementById("sample_site_div").className += " highlight";
         valid = false;
