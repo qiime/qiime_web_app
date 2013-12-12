eddf822f26f7fbd37ba9fd604ac372c639555405
teravest <ejteravest@gmail.com>
2013-12-12 15:57:07 -0700

    fix #549
    the code parses though an obo file and then create 2 output files, an
    sql file and a reltaionship text file

---
+++ support_files/update_ontology_database.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# File created on 27 Nov 2013
+from __future__ import division
+
+__author__ = "Emily TerAvest"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["Emily TerAvest"]
+__license__ = "GPL"
+__version__ = "1.7.0-dev"
+__maintainer__ = "Emily TerAvest"
+__email__ = "ejteravest@gmail.com"
+__status__ = "Development"
+
+
+from qiime.util import parse_command_line_parameters, make_option
+#import sqlite3 
+from data_access_connections import data_access_factory
+from enums import ServerConfig
+
+
+
+
+script_info = {}
+script_info['brief_description'] = "this script takes in a obo file and "+\
+            "update the ontology database"
+script_info['script_description'] = "takes an obo file downloaded from " +\
+            "bioportal.bioontology.org or the ontology website. This script " +\
+            "then parses the obo file and then adds terms not in the ontology"+\
+            "database to the database"
+script_info['script_usage'] = [("","","")]
+script_info['output_description']= "this script has no output"
+script_info['required_options'] = [\
+ # Example required option
+ make_option('-i','--input_fp',type="existing_filepath",help='obo filepath'),\
+ make_option('-O', '--ontology', type="string", help='ontology short name'),\
+ make_option('-o', '--sql_output_fp', type="new_filepath", help='sql output file'),\
+ make_option('-r', '--relationship_output', type="new_filepath", help='relationship'\
+             'output file'),\
+]
+script_info['optional_options'] = [\
+ # Example optional option
+ #make_option('-o','--output_dir',type="new_dirpath",help='the output directory [default: %default]'),\
+]
+script_info['version'] = __version__
+
+
+
+def main():
+
+    data_access = data_access_factory(ServerConfig.data_access_type)
+    con = data_access.getOntologyDatabaseConnection()
+    option_parser, opts, args =\
+       parse_command_line_parameters(**script_info)
+    #onto_id = '946975161'
+    ontology_query = "select ontology_id from ontology where shortname = '%s'" \
+                    % opts.ontology
+    results = con.cursor().execute(ontology_query)
+    ontology_ids = [row[0] for row in results][0]
+    ontology_id = ontology_ids
+    
+    onto_namespace = opts.ontology
+    query_string = "select \"IDENTIFIER\" from  term where ontology_id ="\
+                    " \'%s\'" % ontology_id 
+    results = con.cursor().execute(query_string)
+    exsisting_terms = [row[0] for row in results]
+    
+    #term_query = "select term_id from term where \"IDENTIFIER\" ="\
+    #                        " 'IS_A' and ontology_id = '%s'" % ontology_id
+    
+    #results = con.cursor().execute(term_query)
+    #is_a_id = [row[0] for row in results][0]
+    
+    obo_file = opts.input_fp
+    obo = open(obo_file, "r")
+    line = obo.readline()
+    sqlfile = open(opts.sql_output_fp, 'w')
+    relationshipfile = open(opts.relationship_output, 'w')
+    count = 0
+
+
+    while(line):
+        if line == "[Term]\n":
+            #read in lines and save info until blank line
+            count = count + 1
+            term_values = {'def': ['null'] }
+            term_line = obo.readline()
+            while(term_line != "\n"):
+                #split the line only on the first colon
+                t_key, t_value = term_line.split(': ', 1)
+                if t_key in term_values and t_key != 'def':
+                    term_values[t_key].append(t_value.strip())
+                else:
+                    term_values[t_key] = [t_value.strip()]
+
+                term_line = obo.readline() 
+            #now we need to add term_values to the database
+           
+            if not (term_values['id'][0] in exsisting_terms):
+                insert_stmt = "Insert into Term (ontology_id, term_name, " \
+                    "IDENTIFIER, definition, namespace, is_obsolete, " \
+                    "is_root_term, is_leaf) values ('%s', '%s', '%s', '%s', '%s', 0, 0, 0);\n" \
+                    % (ontology_id, term_values['name'][0].replace('\'','\'\''), term_values['id'][0], \
+                    term_values['def'][0].split('\" ')[0].strip('\"').replace('\'','\'\''), onto_namespace)
+                sqlfile.write(insert_stmt)
+                if 'is_a' in term_values:  
+                    #find the other term in the relationship
+                    object_terms = [a.split(' ! ')[0] for a in term_values['is_a']]
+                    #find the id of that term in the database
+                    #get the id for the term we are working on now
+                    #term_id_stmt = "select term_id from term where \"IDENTIFIER\"" \
+                    #             "'%s'" % term_values['id']
+                    for obj_term in object_terms:
+                    #    obj_id_stmt = "select term_id from term where \"IDENTIFIER\" = '%s'" \
+                    #        % obj_term
+                        relation_string = "%s IS_A %s\n" % (term_values['id'][0], \
+                                            obj_term)
+                        relationshipfile.write(relation_string)
+                        #results = con.cursor().execute(obj_id_stmt)
+                        #obj_term_id = [row[0] for row in results]
+                        #for row in results:
+                        #    print row[0]
+                        #if len(obj_term_id) > 1:
+                            #print obj_term, obj_term_id
+                        #    pass
+                    #    reltaionship_stmt = "insert into term_relationship\n"  
+
+                if 'relationship' in term_values:
+                    relations = [a.split(' ') for a in term_values['relationship']]
+                    for relation in relations:
+                        relation_string = "%s %s %s\n" % (term_values['id'][0], \
+                                        relation[0], relation[1])
+                        relationshipfile.write(relation_string)
+            if 'is_obsolete' in term_values:
+                update_stmt = "update term set is_obsolete = 1 where "\
+                "\"IDENTIFIER\" = '%s';\n" % term_values['id'][0]
+                sqlfile.write(update_stmt)   
+
+            
+        line = obo.readline()
+    print count     
+    obo.close()
+    sqlfile.write("commit;\n")
+    sqlfile.close()
+    con.close()
+    return
+
+if __name__ == "__main__":
+    main()
