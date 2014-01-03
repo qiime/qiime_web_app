b08f5eb59e2d062f94008ac7401aa0d036fa7a67
teravest <ejteravest@gmail.com>
2014-01-03 14:26:53 -0700

    fix changes based on comments

--- www/js/template.js
+++ www/js/template.js
@@ -48,12 +48,14 @@ function validateCorrectedMetadata()
         if (e.style.backgroundColor == invalid_color)
         {
             //check to see if its an ontology we missed the first time though
+            //setting async to false because we want to wait before checking
+            //the result
             is_valid = reValidateOntology(e, false);
             if(is_valid === false)
-                {
+            {
                 alert('Please correct all errors before submitting your metadata.');
                 return false;
-                }
+            }
         }
     }
 }
@@ -363,16 +365,23 @@ function reValidateOntology(sender, wait)
     onto_term = sender.value;
     column_name = sender.id;
     onto_term = "term="+onto_term +"&column_name="+column_name;
+    //if we are coming from the submit button wait will be false
+    //if we are coming from the form onblur wait will be undefined
+    //so we need to set it to the default value for asynch which is true
     wait = wait===undefined?true:wait;
     is_valid = false;
     $.ajax({type: "POST", async: wait, url: "revalidate_ontology.psp", data: onto_term,
         success: function( data ){
             if ( $.trim(data)=='True')
+            {
+                is_valid = true;
+                sender.style.background = valid_color;
+            }
+            else
                 {
-                    is_valid = true;
-                    sender.style.background = valid_color;
+                    sender.style.background = invalid_color;
                 }
-            else { sender.style.background = invalid_color;}},
-                  });
+        }, //end of success
+    });
     return is_valid;
 }
