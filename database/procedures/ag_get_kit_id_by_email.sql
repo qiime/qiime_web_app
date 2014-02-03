9e37b0ae663cbf3fce6e8f08932313fff267cae0
teravest <ejteravest@gmail.com>
2014-02-03 08:53:37 -0700

    Addressed comments form @adamrp and @eldeveloper
    My only other thought it should we only do this after the kit
    has been verified?

---
+++ database/procedures/ag_get_kit_id_by_email.sql
@@ -0,0 +1,18 @@
+create or replace PROCEDURE AG_GET_KIT_ID_BY_EMAIL(
+      email_ in varchar2,
+      user_data_ OUT types.ref_cursor
+)AS 
+BEGIN
+     open user_data_ for
+         select  k.supplied_kit_id 
+         from ag_kit k 
+              inner join ag_login l 
+              on k.ag_login_id = l.ag_login_id 
+         where l.email = email_; 
+
+END AG_GET_KIT_ID_BY_EMAIL;
+
+/*variable user_data_ REFCURSOR;
+execute ag_get_kits_by_login('ejteravest@gmail.com', :user_data_);
+print user_data_;
+*/
