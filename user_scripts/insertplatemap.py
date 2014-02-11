e95a782914841de8926329889b1a957f7d15e700
teravest <ejteravest@gmail.com>
2014-02-11 12:54:41 -0700

    First draft of barcode tracking updates
    This is the first draft of barcode tracking updates
    It can be merged as is then built onto for other updates
    insterplatemap.py takes the AG platemap and outputs plateinsert.sql
    updatebarcoderesults.py takes a list of resultpdfs and outputs updateresults.sql
    Tracking.sql is all the sql changes I made to the database for this.
    ag_search.psp has been updated to display the new information
    
    Database Changes
    New table PLATE -  currently stores a unique plate id, and the plate name, and the date sequenced
    	possible additions are robot_used, reagent, etc
    New table PLATE_BARCODE currently stores plate_id and barcode
    	possible additions include well id
    Barcode had 4 fields added biomass_remaining, sequencing_status, extracted, obsolete
    
    Ag_kit_barcodes had 3 fields added restuls_ready, withdrawn and refunded

---
+++ user_scripts/insertplatemap.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# File created on 07 Feb 2014
+from __future__ import division
+
+__author__ = "Emily TerAvest"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["Emily TerAvest"]
+__license__ = "GPL"
+__version__ = "1.7.0-dev"
+__maintainer__ = "Emily TerAvest"
+__email__ = "ejteravest@gmail.com"
+__status__ = "Development"
+
+
+from qiime.util import parse_command_line_parameters, make_option
+
+script_info = {}
+script_info['brief_description'] = """Take a plate map and creates db insert
+statements for plate and plate_barcode table"""
+script_info['script_description'] = """Input american gut plate map, sql output file"""
+script_info['script_usage'] = [("Examples:","create db insert statment","%prog -i ag_plate_map.txt -o ag_plate_inserts.sql")]
+script_info['output_description']= "sql file"
+script_info['required_options'] = [\
+ # Example required option
+make_option('-i','--input_fp',type="existing_filepath",help='the input filepath'),\
+make_option('-o','--output_fp', type="new_filepath",help='the output filename')\
+]
+script_info['optional_options'] = []
+script_info['version'] = __version__
+
+
+
+def main():
+    option_parser, opts, args =\
+       parse_command_line_parameters(**script_info)
+    platemapfile=opts.input_fp
+    sqlfile=opts.output_fp
+    sqloutfile=open(sqlfile, 'w')
+    platemaplines = open(platemapfile,'r').readlines()
+    platename=''
+    print len(platemaplines)
+    for line in platemaplines:
+        #print line
+        line = line.split(',')
+        if 'Plate' in line[0] and line[1] != '1':
+            #new plate we only care about the first line
+            platename=line[0]
+            sqlstmt = "insert into plate (plate_id, plate) values (seq_plate_id.nextval, '%s');\n" % platename
+            sqloutfile.write(sqlstmt)
+        elif line[0] != '':
+            #this line contains barcodes
+            if len(line[0]) == 1:
+                for barcode in line[1:]:
+                    if barcode != '':
+                        try:
+                            barcode = int(barcode)
+                        except:
+                            continue
+                        if barcode > 1000:
+                            #create an insert statment
+                            sqlstmt = "insert into plate_barcode (plate_id, barcode) select plate_id, '%09d' from plate where plate = '%s';\n" % (barcode, platename)
+                            sqloutfile.write(sqlstmt)
+        else:
+            #this is a separator between plates reset platename 
+            platename = ''
+
+    sqloutfile.close()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ user_scripts/updatebarcoderesults.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# File created on 11 Feb 2014
+from __future__ import division
+
+__author__ = "Emily TerAvest"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["Emily TerAvest"]
+__license__ = "GPL"
+__version__ = "1.7.0-dev"
+__maintainer__ = "Emily TerAvest"
+__email__ = "ejteravest@gmail.com"
+__status__ = "Development"
+
+
+from qiime.util import parse_command_line_parameters, make_option
+
+script_info = {}
+script_info['brief_description'] = """Take a list of all the barcdoes we have
+american gut results for and updates the database to reflect having results"""
+script_info['script_description'] = """Takes a text file of the results of ls 
+the american gut results directory and outputs an sql file to update the database"""
+script_info['script_usage'] = [("Examples:","create database update statements","%prog -i listoffiles.txt -o updateresults.sql")]
+script_info['output_description']= "an sql file to update results read status of db"
+script_info['required_options'] = [\
+ # Example required option
+ make_option('-i','--input_fp',type="existing_filepath",help='the input filepath a list of all the files in the results directory'),\
+ make_option('-o','--output_fp',type="new_filepath",help='the output sqlfile')
+]
+script_info['optional_options'] = [\
+ # Example optional option
+ #make_option('-o','--output_dir',type="new_dirpath",help='the output directory [default: %default]'),\
+]
+script_info['version'] = __version__
+
+
+
+def main():
+    option_parser, opts, args =\
+       parse_command_line_parameters(**script_info)
+    filenamesfilename=opts.input_fp
+    filenamesfile = open(filenamesfilename, 'r')
+    sqlfile=opts.output_fp
+    sqloutfile=open(sqlfile, 'w')
+    #barcodeline = barcodeslist.readline()
+    barcodeline = filenamesfile.readline()
+    while (barcodeline):
+        barcode = barcodeline.split(' ')[-1].split('.')[0]
+        sqlstmt = "update ag_kit_barcodes set results_ready = 'Y' where barcode = '%s';\n" % barcode
+        sqloutfile.write(sqlstmt)
+        sqlstmt = "update barcode set sequencing_status = 'SUCCESS' where barcode = '%s';\n" % barcode
+        sqloutfile.write(sqlstmt)
+        barcodeline = filenamesfile.readline()
+    sqloutfile.close()
+    filenamesfile.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
