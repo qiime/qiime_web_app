e79330bc12e955d91ecb320595bf50bfe2595ada
teravest <ejteravest@gmail.com>
2014-03-07 13:54:08 -0700

    change sender on emails

--- python_code/utils/mail.py
+++ python_code/utils/mail.py
@@ -5,7 +5,7 @@ from __future__ import division
 __author__ = "Yoshiki Vazquez-Baeza"
 __copyright__ = "Copyright 2009-2013, QIIME Web Analysis"
 __credits__ = ["Meg Pirrung", "Adam Robbins-Pianka", "Yoshiki Vazquez-Baeza",
-    "Daniel McDonald"]
+    "Daniel McDonald", "Emily TerAvest"]
 __license__ = "GPL"
 __version__ = "1.0.0.dev"
 __maintainer__ = ["Yoshiki Vazquez-Baeza"]
@@ -29,7 +29,7 @@ def can_send_mail():
     """
     return 'microbio.me' in gethostname()
 
-def send_email(message, subject, recipient='americangut@gmail.com'):
+def send_email(message, subject, recipient='americangut@gmail.com', sender=FROM_EMAIL):
     """Send an email from your local host """
 
     # Create a text/plain message
@@ -38,11 +38,11 @@ def send_email(message, subject, recipient='americangut@gmail.com'):
     # me == the sender's email address
     # you == the recipient's email address
     msg['Subject'] = subject
-    msg['From'] = FROM_EMAIL
+    msg['From'] = sender
     msg['To'] = recipient
 
     # Send the message via our own SMTP server, but don't include the
     # envelope header.
     s = smtplib.SMTP('localhost')
-    s.sendmail(FROM_EMAIL, [recipient], msg.as_string())
+    s.sendmail(sender, [recipient], msg.as_string())
     s.quit()
