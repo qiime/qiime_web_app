134f47dd140cd030051ebf87838a5ee7a1800396
Emily TerAvest <ejteravest@gmail.com>
2014-04-25 15:51:05 -0600

    add procedure

---
+++ database/procedures/ag_get_print_results.sql
@@ -0,0 +1,9 @@
+reate or replace procedure ag_get_print_results(
+   kit_id_  in varchar2,
+   results_ in out types.ref_cursor
+)as 
+begin
+  open results_ for 
+  select print_results from ag_handout_kits 
+  where kit_id = kit_id_;
+end ag_get_print_results;
